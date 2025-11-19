42084ec58f96066145964c97015525e99a657c93
가동재 <masterforce999@naver.com>
2025-11-19 15:22:47 +0900

    리드미 테스트

--- src/blocks/dashboard-02/page.tsx
+++ src/blocks/dashboard-02/page.tsx
@@ -1,3 +1,27 @@
+import {
+  Dialog,
+  DialogTrigger,
+  DialogHeader,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+} from "@hdc-ui/components/ui/dialog";
+import { Button } from "@hdc-ui/components/ui/button";
+
+import { cn } from "@hdc-ui/utils";
+
 export default function Page() {
-  return <div></div>;
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>버튼</Button>
+      </DialogTrigger>
+      <DialogContent className={cn("w-100")}>
+        <DialogHeader>
+          <DialogTitle>타이틀</DialogTitle>
+        </DialogHeader>
+        <DialogDescription>설명</DialogDescription>
+      </DialogContent>
+    </Dialog>
+  );
 }
